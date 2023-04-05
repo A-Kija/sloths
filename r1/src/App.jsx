7ff6829df765cea674c964f62f61083ecbe58816
A-Kija <arvydas.kijakauskas@gmail.com>
2023-04-05 16:57:10 +0300

    props

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,19 +1,39 @@
-import './App.css';
-import './Components/005/animal.scss';
-import Fox from './Components/005/Fox';
-import Racoon from './Components/005/Racoon';
+import './App.scss';
+import Square from './Components/006/Square';
+
+const data = [
+    {color: '#87ceeb', letter: 1},
+    {color: '#dc143c', letter: 5},
+    {color: '#dc143c', letter: 7},
+    {color: '#87aaeb', letter: 22},
+    {color: '#6495ed', letter: 3},
+    {color: '#87aaeb', letter: 1}
+   
+];
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-
-        <h1>START</h1>
-
-        <Racoon />
-        <Fox />
-
+      
+        <div className="squares">
+            {
+                data.map((d, index) => <Square key={index} niceColor={d.color} letter={d.letter} />)
+            }
+        </div>
+        <div className="squares">
+            {
+                data.map((d, index) => d.letter > 5 
+                ? null 
+                : <Square key={index} niceColor={d.color} letter={d.letter} />)
+            }
+        </div>
+        <div className="squares">
+            {
+                [...data].sort((a, b) => b.letter - a.letter).map((d, index) => <Square key={index} niceColor={d.color} letter={d.letter} />)
+            }
+        </div>
 
       </header>
     </div>
