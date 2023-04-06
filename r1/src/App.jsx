9d0ea0a257bb359765790c47132fbf219ec902d1
A-Kija <arvydas.kijakauskas@gmail.com>
2023-04-06 17:30:13 +0300

    state

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,56 +1,118 @@
+import { useState } from 'react';
 import './App.scss';
-import Square from './Components/006/Square';
+import Square from './Components/Bond/Square';
+import rand from './Functions/rand';
+import randColor from './Functions/randColor';
 
-const data = [
-    {color: '#87ceeb', letter: 1},
-    {color: '#dc143c', letter: 5},
-    {color: '#dc143c', letter: 7},
-    {color: '#87aaeb', letter: 22},
-    {color: '#6495ed', letter: 3},
-    {color: '#87aaeb', letter: 1}
-   
-];
 
 function App() {
 
-  const print = color => {
-    console.log('%c' + color, `background:${color}; padding:5px;`);
-  }
+    // const [count, setCount] = useState(7);
+    // const [letter, setLetter] = useState('NOP')
 
-  return (
-    <div className="App">
-      <header className="App-header">
-      
-        <div className="squares">
-            {
-                data.map((d, index) => <Square key={index} niceColor={d.color} letter={d.letter} />)
-            }
-        </div>
-        <div className="squares">
-            {
-                data.map((d, index) => d.letter > 5 
-                ? null 
-                : <Square key={index} niceColor={d.color} letter={d.letter} />)
-            }
-        </div>
-        <div className="squares">
-            {
-                [...data].sort((a, b) => b.letter - a.letter).map((d, index) => <Square key={index} niceColor={d.color} letter={d.letter} />)
-            }
-        </div>
-        <div className="squares">
-          <button onClick={_ => print('gray')}>Gray</button>
-          <button className="red" onClick={_ => print('crimson')}>Red</button>
-          <button className="blue" onClick={_ => print('skyblue')}>Blue</button>
-          <button className="pink" onClick={_ => print('pink')}>Pink</button>
-          <button className="yellow" onClick={_ => print('darkorange')}>Yellow</button>
-          <button className="green" onClick={_ => print('greenyellow')}>Green</button>
+    // const set0 = _ => {
+    //     setCount(0); // NAUJAS PAPRASTAI
+    // }
+
+    // const plus1 = _ => {
+    //     // setCount(count + 1); // IVYKSTA NE IS KARTO!!!!!
+    //     // setCount(count + 1);
+    //     // setCount(count + 1);
+
+    //     setCount(c => c + 1); // REDAGAVIMAS TIK PER CALLBACK
+    //     setCount(c => c + 1); 
+    //     setCount(c => c + 1); 
+
+    // }
+
+    const [sq, setSq] = useState([]);
+
+    const addSq = _ => {
+        setSq(s => [...s, {
+            color: randColor(),
+            spin: rand(0, 1) ? 'spin' : 'spin-back',
+            number: ('' + rand(1, 9999)).padStart(4, '0'),
+            row: s.length,
+            show: true
+        }
+        ]);
+        
+    }
+
+    const sort = _ => {
+        const direction =
+            (sq[0]?.number || 0) - (sq[sq.length - 1]?.number || 0) > 0
+                ? 1 : -1;
+        setSq(s => [...s].sort((a, b) => direction * (a.number - b.number)));
+    }
+
+    const sortDefault = _ => {
+        setSq(s => [...s].sort((a, b) => a.row - b.row));
+    }
+
+    const l1000 = _ => {
+        if (-1 === sq.findIndex(s => s.show === false)) {
+            setSq(s => s.map(s => s.number < 1000 ? {...s, show: true} : {...s, show: false}));
+        } else {
+            setSq(s => s.map(s => ({...s, show: true})));
+        }
+        
+    }
+
+    const spin = dir => {
+        switch(dir) {
+            case 'r':
+                setSq(s => s.map(s => s.spin === 'spin' ? {...s, show: true} : {...s, show: false}));
+                break;
+            case 'l':
+                setSq(s => s.map(s => s.spin === 'spin-back' ? {...s, show: true} : {...s, show: false}));
+                break;
+            default:
+                setSq(s => s.map(s => ({...s, show: true})));  
+        }
+    }
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {/* <div className="squares">
+
+            <Square number={count}/>
+            <Square number={letter}/>
 
         </div>
+        <div className="squares">
+        <button className="yellow" onClick={set0}>0</button>
+        <button className="pink" onClick={_=> setLetter(l => l === 'PINK' ? 'NOP' : "PINK" )}>pink</button>
+        <button className="red" onClick={plus1}>+1</button>
+        </div> */}
 
-      </header>
-    </div>
-  );
+
+                <div className="squares">
+                    {
+                        sq.map((s, i) => s.show ? <Square key={i} data={s} /> : null)
+                    }
+                </div>
+
+                {console.log(sq)}
+
+                <div className="squares">
+                    <button className="green" onClick={addSq}>add []</button>
+                    <button className="red" onClick={_ => setSq([])}>destroy all</button>
+                    <button className="blue" onClick={sort}>sort</button>
+                    <button className="blue" onClick={sortDefault}>Deafault Sort</button>
+                    <button className="yellow" onClick={l1000}>LESS 1000</button>
+                    <button className="pink" onClick={_ => spin('l')}>LEFT SPIN</button>
+                    <button className="pink" onClick={_ => spin('r')}>RIGHT SPIN</button>
+                    <button className="pink" onClick={_ => spin('b')}>BOTH SPIN</button>
+                </div>
+
+
+
+            </header>
+        </div>
+    );
 
 }
 
