5dd5a26c48f958e565f6946bd0fd5e6c5ec7a45b
A-Kija <arvydas.kijakauskas@gmail.com>
2023-04-13 14:11:02 +0300

    farm task

---
+++ r1/src/Components/010/Ganykla.jsx
@@ -0,0 +1,68 @@
+import rand from '../../Functions/rand';
+import './farm.scss';
+import Gyvulys from './Gyvulys';
+import { useEffect, useState } from 'react';
+function Ganykla() {
+
+    const [animals, setAnimals] = useState(null);
+
+    const spring = _ => {
+        const all = [];
+        ['avis', 'karvė'].forEach((type, index) => {
+            const count = rand(2, 7);
+            for (let i = 0; i < count; i++) {
+                let id = ('' + rand(0, 9999999)).padStart(7, '0');
+                id = type === 'avis' ? 'A' + id : 'K' + id;
+                all.push({ id, type, number: index });
+            }
+        });
+        setAnimals(all);
+    }
+
+    useEffect(() => {
+        if (null !== localStorage.getItem('farm')) {
+            setAnimals(JSON.parse(localStorage.getItem('farm')))
+        } else {
+            setAnimals([]);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (null === animals) {
+            return;
+        }
+        localStorage.setItem('farm', JSON.stringify(animals));
+    }, [animals]);
+
+
+    if (null === animals) {
+        return null;
+    }
+
+    return (
+        <>
+            <div className="ganykla">
+                <div className="dalis">
+                    <h2>Avys</h2>
+                    <div className="tvora">
+                        {
+                            animals.map(a => a.number === 0 ? <Gyvulys key={a.id} apie={a} setAnimals={setAnimals} /> : null)
+                        }
+                    </div>
+                </div>
+                <div className="dalis">
+                    <h2>Karvės</h2>
+                    <div className="tvora">
+                        {
+                            animals.map(a => a.number === 1 ? <Gyvulys key={a.id} apie={a} setAnimals={setAnimals}/> : null)
+                        }
+                    </div>
+                </div>
+            </div>
+            <button className="yellow" onClick={spring}>į ganyklą</button>
+        </>
+    );
+
+}
+
+export default Ganykla;
---
+++ r1/src/Components/010/Gyvulys.jsx
@@ -0,0 +1,18 @@
+
+function Gyvulys({apie, setAnimals}) {
+
+    const run = _ => {
+        setAnimals(a => a.map(a => a.id === apie.id ? {...a, number: a.number === 0 ? 1 : 0} : {...a}))
+    }
+
+    return (
+        <div className="gyvulys" style={{
+            borderRadius: apie.type === 'avis' ? '50%' : null
+        }} onClick={run}>
+            {apie.id}
+        </div>
+    );
+
+}
+
+export default Gyvulys;
