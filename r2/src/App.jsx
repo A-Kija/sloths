933b025752fe378e40e59395b0888dea9cd43adf
A-Kija <arvydas.kijakauskas@gmail.com>
2023-04-18 15:31:08 +0300

    list

--- r2/src/App.jsx
+++ r2/src/App.jsx
@@ -1,8 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.scss';
 import Create from './Components/Create';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { crudCreate, crudRead } from './Utils/localStorage';
+import List from './Components/List';
+
+const key = 'ClientsDb';
 
 function App() {
+
+  const [data, setData] = useState(null);
+  const [createData, setCreateData] = useState(null);
+
+
+  useEffect(() => {
+    setData(crudRead(key));
+  }, []);
+
+  useEffect(() => {
+    if (null === createData) {
+      return;
+    }
+    crudCreate(key, createData);
+
+  }, [createData]);
+
   return (
     <>
       <nav className="navbar navbar-light bg-light">
@@ -13,10 +36,10 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col-4">
-            <Create />
+            <Create setCreateData={setCreateData} />
           </div>
           <div className="col-8">
-            LIST
+            <List data={data}/>
           </div>
         </div>
       </div>
