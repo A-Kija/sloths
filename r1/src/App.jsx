070e46f7d378e316269450dc1aa143b7364537a0
A-Kija <arvydas.kijakauskas@gmail.com>
2023-04-20 15:22:59 +0300

    useRef

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,106 +1,61 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
 import './App.scss';
-import ContentEditable from 'react-contenteditable';
-
-const list = [
-    { id: 5460, name: 'Beaver' },
-    { id: 5456, name: 'Fox' },
-    { id: 8746, name: 'Rabbit' },
-    { id: 5879, name: 'Wolf' }
-];
+import { useRef, useState } from 'react';
 
 function App() {
 
-    const [text, setText] = useState({
-        t1: 'Labas',
-        t2: 'Ate'
-    });
+    const [count, setCount] = useState(1);
 
-    const [checkbox, setCheckbox] = useState({
-        A: true,
-        B: false,
-        C: true,
-        D: false
-    });
+    const needFocus = useRef();
 
-    const [radio, setRadio] = useState('C');
+    const mySave = useRef(1);
 
-    const [select, setSelect] = useState({open: false, data: 8746});
+    // const [mySave2, setMySave2] = useState(1);
+    
+    
+    useEffect(() => {
+
+        mySave.current = count;
+        console.log('SAVE:', mySave.current);
+
+    }, [count, mySave])
 
 
+    // useEffect(() => {
 
-    const handleChange1 = (e, t) => {
-        setText(texts => ({ ...texts, [t]: e.target.value }))
+    //     setMySave2(count);
+    //     console.log('SAVE2:', mySave2);
+
+    // }, [count, mySave2])
+
+
+    const click = _ => {
+        setCount(c => c + 1);
+        // document.querySelector('#need-focus').focus();
+        needFocus.current.focus();
+        console.log('CLICK:', mySave.current);
     }
 
-    const handleChange2 = cb => {
-        setCheckbox(checkboxes => ({ ...checkboxes, [cb]: !checkboxes[cb] }));
-    }
 
-    const handleChange3 = r => {
-        setRadio(r);
-    }
+    // const save = _ => {
+    //     mySave.current = count;
+    //     console.log('SAVE:', mySave.current);
+    // }
 
-    const handleChange4 = id => {
-        setSelect({open: false, data: parseInt(id)});
-    }
 
 
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Forms</h1>
-                <fieldset>
-                    <legend>Few texts one state</legend>
-                    <div>
-                        <ContentEditable tagName="i" onChange={e => handleChange1(e, 't1')} html={text.t1} />
-                    </div>
-                    <div>
-                        <ContentEditable tagName="u" onChange={e => handleChange1(e, 't2')} html={text.t2} />
-                    </div>
-                </fieldset>
+                <h1>useRef</h1>
 
-                <fieldset>
-                    <legend>4 checkboxes one state</legend>
-                    <div className="field">
-                        <span className={'checkbox' + (checkbox.A ? ' active' : '')} onClick={_ => handleChange2('A')}>A</span>
-                        <span className={'checkbox' + (checkbox.B ? ' active' : '')} onClick={_ => handleChange2('B')}>B</span>
-                        <span className={'checkbox' + (checkbox.C ? ' active' : '')} onClick={_ => handleChange2('C')}>C</span>
-                        <span className={'checkbox' + (checkbox.D ? ' active' : '')} onClick={_ => handleChange2('D')}>D</span>
-                    </div>
-                </fieldset>
+                <input id="need-focus" ref={needFocus} type="text" value={count}></input>
 
-                <fieldset>
-                    <legend>4 checkboxes one state</legend>
-                    <div className="field">
-                        <span className={'checkbox' + (radio === 'A' ? ' active' : '')} onClick={_ => handleChange3('A')}>A</span>
-                        <span className={'checkbox' + (radio === 'B' ? ' active' : '')} onClick={_ => handleChange3('B')}>B</span>
-                        <span className={'checkbox' + (radio === 'C' ? ' active' : '')} onClick={_ => handleChange3('C')}>C</span>
-                        <span className={'checkbox' + (radio === 'D' ? ' active' : '')} onClick={_ => handleChange3('D')}>D</span>
-                    </div>
-                </fieldset>
+                <button className="red" onClick={click}>Click Me!</button>
 
-                <fieldset>
-                    <legend>Select on one state</legend>
-
-                    <div className="select">
-                        <div className="selected" onClick={_ => setSelect(s => ({...s, open: !s.open}))}>{list.find(l => select.data === l.id).name}</div>
-                        <div className="list" style={{height: select.open ? '160px' : 0}} >
-                            {
-                                list.map(o => <div key={o.id} onClick={_ => handleChange4(o.id)}>{o.name}</div>)
-                            }
-                        </div>
-
-                    </div>
-
-
-                </fieldset>
-
-
-
+                {/* <button className="blue" onClick={save}>Save</button> */}
 
             </header>
-
         </div>
     );
 
