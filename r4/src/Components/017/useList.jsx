c7bd8634b02e2f459c2010166603b331535bf663
A-Kija <arvydas.kijakauskas@gmail.com>
2023-04-27 13:39:09 +0300

    Custom Hook

---
+++ r4/src/Components/017/useList.jsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+export default function useList() {
+
+    const [list, setList] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://dummyjson.com/quotes')
+        .then(res => setList(res.data.quotes));
+    }, []);
+
+    return [list];
+
+}
