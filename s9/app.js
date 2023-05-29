e65635f65ad5e9bb0ba4815899615dc12124c204
A-Kija <arvydas.kijakauskas@gmail.com>
2023-05-29 10:15:25 +0300

    w/ files

--- s9/app.js
+++ s9/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 // const cookieParser = require('cookie-parser');
-// const fs = require('fs');
-// const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 // const md5 = require('md5');
 
 const app = express();
 const port = 3003;
-app.use(express.json({limit: '10mb'}));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.static('public'))
 app.use(cors({
     origin: 'http://localhost:3000',
@@ -35,7 +35,7 @@ connection.connect();
 app.get('/images', (_, res) => {
     const sql = `
         SELECT *
-        FROM photos
+        FROM images
     `;
     connection.query(sql, (err, result) => {
         if (err) throw err
@@ -48,11 +48,41 @@ app.get('/images', (_, res) => {
 
 
 app.post('/images', (req, res) => {
+
+    let fileName = null;
+
+    if (req.body.file !== null) {
+
+        let type = 'unknow';
+        let file;
+
+        if (req.body.file.indexOf('data:image/jpeg;base64,') === 0) {
+            type = 'jpg';
+            file = Buffer.from(req.body.file.replace('data:image/jpeg;base64,', ''), 'base64');
+
+        } else if (req.body.file.indexOf('data:image/png;base64,') === 0) {
+            type = 'png';
+            file = Buffer.from(req.body.file.replace('data:image/png;base64,', ''), 'base64');
+
+        } else if (req.body.file.indexOf('data:image/gif;base64,') === 0) {
+            type = 'gif';
+            file = Buffer.from(req.body.file.replace('data:image/gif;base64,', ''), 'base64');
+
+        } else {
+            file = Buffer.from(req.body.file, 'base64');
+        }
+
+        fileName = uuidv4() + '.' + type;
+
+        fs.writeFileSync('./public/img/' + fileName, file);
+
+    }
+
     const sql = `
-    INSERT INTO photos (title, file)
+    INSERT INTO images (title, url)
     VALUES (?, ?)
     `;
-    connection.query(sql, [req.body.title, req.body.file ? req.body.file : null], (err, _) => {
+    connection.query(sql, [req.body.title, fileName], (err, _) => {
         if (err) throw err
         res.json({
             status: 'ok',
@@ -62,8 +92,24 @@ app.post('/images', (req, res) => {
 
 
 app.delete('/images/:id', (req, res) => {
-    const sql = `
-        DELETE FROM photos
+
+    let sql;
+
+    sql = `
+    SELECT url
+    FROM images
+    WHERE id = ?
+    `;
+
+    connection.query(sql, [req.params.id], (err, result) => {
+        if (err) throw err
+        if (result[0] && result[0].url) {
+            fs.unlinkSync('./public/img/' + result[0].url)
+        }
+    });
+
+    sql = `
+        DELETE FROM images
         WHERE id = ?
     `;
     connection.query(sql, [req.params.id], (err, _) => {
